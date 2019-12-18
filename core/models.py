b81326d0af9ab17a32f4f1ad12960d9555826da3
OH-JOO-YEONG <brb1111@naver.com>
2019-12-19 05:30:16 +0900

    Room Model and Admin done

---
+++ core/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+
+class TimeStampedModel(models.Model):
+    """ Time Stamped Model """
+    created = models.DateTimeField(auto_now_add=True)
+    upadted = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True # 데이터베이스에 나타나지 않는 모델을 설정하는 법(추상모댈)
--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django_countries.fields import CountryField
+from core import models as core_models
+from users import models as user_models
 
-# Create your models here.
+class AbstractItem(core_models.TimeStampedModel):
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+class RoomType(AbstractItem):
+    """ RoomType Model Definition """
+
+    class Meta:
+        verbose_name = "Room Type"
+
+class Amenity(AbstractItem):
+    """ Amenity Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+class Facility(AbstractItem):
+    """ Facility Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+class HouseRule(AbstractItem):
+    """ HouseRule Model Definition """
+
+    class Meta:
+        verbose_name = "House Rule"
+
+class Photo(core_models.TimeStampedModel):
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE) # 포린키를 String으로 연결하는 방법도 있다.(모델이 많거나, 위에서 아래로 읽는 파이썬의 방식이 마음에 안들면 쓴다.
+
+    def __str__(self):
+        return self.caption
+
+class Room(core_models.TimeStampedModel):
+    """ Room Model Definition"""
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=140)
+    guests = models.IntegerField()
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE) # 다른 모델에 있는 것을 String으로 연결할 때는 "users.user" 로 연결
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
