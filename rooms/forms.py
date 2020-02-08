e38298a1566e0b9223124e3edfe46d4830a3c8e1
OH-JOO-YEONG <brb1111@naver.com>
2020-02-08 19:52:31 +0900

    Search func, made of SearchForm and search func turn into SearchView

---
+++ rooms/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django_countries.fields import CountryField
+from . import models
+
+class SearchForm(forms.Form):
+    city = forms.CharField(initial="Anywhere")
+    country = CountryField(default="KR").formfield()
+    room_type = forms.ModelChoiceField(required=False, empty_label="Any Kind", queryset=models.RoomType.objects.all())
+    price = forms.IntegerField(required=False)
+    guests = forms.IntegerField(required=False)
+    bedrooms = forms.IntegerField(required=False)
+    beds = forms.IntegerField(required=False)
+    baths = forms.IntegerField(required=False)
+    instant_book = forms.BooleanField(required=False)
+    superhost = forms.BooleanField(required=False)
+    amenities = forms.ModelMultipleChoiceField(required=False, queryset=models.Amenity.objects.all(), widget=forms.CheckboxSelectMultiple)
+    facilities = forms.ModelMultipleChoiceField(required=False, queryset=models.Facility.objects.all(), widget=forms.CheckboxSelectMultiple)
+
+    # 폼을 가지고자 하는 목적
+    """
+    HTML을 엄청 빨리 만들어준다.
+    데이터를 다 정리해준다.(이상한 데이터들을 필드에 와서 int형인지 str형인지 정리시켜줌)
+    
+    """
