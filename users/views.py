ba9402b2914e8bce71f107c3caf4eb13c1bd9002
OH-JOO-YEONG <brb1111@naver.com>
2020-02-19 00:07:15 +0900

    # Comment organized and GithubException

--- users/views.py
+++ users/views.py
@@ -71,16 +71,47 @@ def github_login(request):
     client_id = os.environ.get("GH_ID")
     redirect_uri = "http://127.0.0.1:8000/users/login/github/callback"
     return redirect(f'https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user')
+    #유저가 리다이렉트되고
+    #리다이렉트 된 유저는 github페이지로 가서 우리가 로그인하고 싶은 어플리케이션의 아이디와 어떤 걸 얻고싶은지 알아내는 기능
 
-def github_callback(requset):
-    client_id = os.environ.get("GH_ID")
-    client_secret = os.environ.get("GH_SECRET")
-    code = requset.GET.get("code", None)
-    if code is not None:
-        requset = requests.post(
-            f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
-            headers={"Accept": "application/json"},
-        )
-        print(requset.json())
-    else:
-        return redirect(reverse("core:home"))
+class GithubException(Exception):
+    pass
+
+def github_callback(request):
+    try:
+        client_id = os.environ.get("GH_ID")
+        client_secret = os.environ.get("GH_SECRET")
+        code = request.GET.get("code", None)
+        if code is not None:
+            result = requests.post(
+                f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
+                headers={"Accept": "application/json"}, #이 줄에 의해서 json 값으로 주어진다.
+            )
+            result_json = result.json()
+            error = result_json.get("error", None) #json 에러 판별
+            if error is not None:
+                raise GithubException()
+            else: # json이 문제가 없을 때 액세스토큰을 준다.
+                access_token = result_json.get("access_token") # 액세스 토큰이 문제 없으면 github api를 요청할 수 있다.
+                profile_request = requests.get(
+                    "https://api.github.com/user",
+                    headers={
+                        "Authorization": f"token {access_token}",
+                        "Accept": "application/json",
+                    },
+                )
+                profile_json = profile_request.json()
+                username = profile_json.get("login", None)
+                # 유저네임이 현재 profile request에에있는지 없는지 판별
+                if username is not None:
+                    name = profile_json.get("name")
+                    email = profile_json.get("email")
+                    bio = profile_json.get("bio")
+                    user = models.User.objects.get(email=email)
+                else:
+                    raise GithubException()
+        else:
+            raise GithubException()
+
+    except GithubException:
+        return redirect(reverse("users:login"))
