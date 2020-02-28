d539579600fadcd33a3e1f80503c97fbddea329e
OH-JOO-YEONG <brb1111@naver.com>
2020-02-29 00:30:12 +0900

    Styling Messages

--- users/views.py
+++ users/views.py
@@ -32,6 +32,7 @@ class LoginView(View):
         })
 
 def log_out(request):
+    messages.info(request, f"See you later {request.user.first_name}")
     logout(request)
     return redirect(reverse("core:home"))
 
