0ace645d66659be8ed12a38cfb15f9c959d8a056
OH-JOO-YEONG <brb1111@naver.com>
2020-03-01 22:56:21 +0900

    Session-based func apply

--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core.files.base import ContentFile
@@ -255,6 +256,13 @@ class UpdatePasswordView(mixins.EmailLoginOnlyView, mixins.LoggedInOnlyView, Suc
     def get_success_url(self):
         return self.request.user.get_absolute_url()
 
+@login_required
+def switch_hosting(request):
+    try:
+        del request.session["is_hosting"]
+    except KeyError:
+        request.session["is_hosting"] = True
+    return redirect(reverse("core:home"))
 
 
 
