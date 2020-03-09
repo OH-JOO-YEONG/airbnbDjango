51214f32226d9c4048e3bddccb4de98bdfa2ebfb
OH-JOO-YEONG <brb1111@naver.com>
2020-03-09 20:14:45 +0900

    Reservation part done and Review part done

--- core/models.py
+++ core/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from . import managers
 
 
 class TimeStampedModel(models.Model):
     """ Time Stamped Model """
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = managers.CustomModelManager()
 
     class Meta:
         abstract = True # 데이터베이스에 나타나지 않는 모델을 설정하는 법(추상모델)
