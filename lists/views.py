6b4a60ae63ec2189dbfe7c8fa3aa7d85ac406fd8
OH-JOO-YEONG <brb1111@naver.com>
2020-04-18 21:21:36 +0900

    Saving Favorite Rooms

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from rooms import models as room_models
+from . import models
 
-# Create your views here.
+def save_room(request, room_pk):
+    room = room_models.Room.objects.get_or_none(pk=room_pk)
+    if room is not None:
+        # get_or_create는 두가지 변수를 지정하지 않고 한가지로 정하면 튜플형태로 나온다. 그래서 _로 unpack해줌
+        the_list, _ = models.List.objects.get_or_create( # get_or_create는 하나만 찾도록 돼있다 하나의 objects만 찾는다 만약 많은 리스트가 있으면 multiple objects error가 발생.
+            user=request.user,
+            name="My Favorite Houses",
+        )
+        the_list.rooms.add(room)
+    return redirect(reverse("rooms:detail", kwargs={
+        "pk": room_pk,
+    }))
